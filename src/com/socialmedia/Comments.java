a4e1f09deaad636ea35ab8f759083bac5048ddd4
Lucía Parrilla del Río <luciaparrilla9@hotmail.com>
2024-11-13 17:31:55 +0100

    WIP

--- src/com/socialmedia/Comments.java
+++ src/com/socialmedia/Comments.java
@@ -3,25 +3,5 @@ package com.socialmedia;
 import java.util.Date;
 
 public class Comments {
-    private String comment;
-    private Date date;
-    private User user;
 
-    public Comments(String texto, User propietario) {
-        this.comment = comment;
-        this.date = new Date(); // Fecha actual
-        this.user = propietario;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public User getUser() {
-        return user;
-    }
 }
